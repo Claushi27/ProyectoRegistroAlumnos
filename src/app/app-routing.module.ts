02320aaa3aa530747126d685ba388480377f69e7
ANDRESGAMBOAT <an.gamboat@duocuc.cl>
2024-10-25 02:20:39 -0300

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home/home.page';
 import { LoginPage } from './login/login.page';
 import { ResetPasswordPage } from './reset-password/reset-password.page';
-import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -14,8 +14,7 @@ const routes: Routes = [
    path: '', 
    redirectTo: 'home',
    pathMatch: 'full'
-  },
-  { path: '**', component: PageNotFoundComponent }
+  }
 ];
 
 @NgModule({
